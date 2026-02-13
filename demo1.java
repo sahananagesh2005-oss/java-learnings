6f8c0e303c2257be19bb823abef6d51439bbb0ef
yashaswini <you@example.com>
2026-02-13 10:10:13 +0530

    completed till demo1

---
+++ demo1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class School{
+    void Display()
+    {
+        System.out.println("Welcome to School");
+    }
+
+    public static void main(String[] args) {
+        School obj=new School();
+        obj.Display();
+    }
+}
+
